a876185cce4e27187b18cebd33cb67608cbdd525
Alan <alanbiegun13@gmail.com>
2018-10-14 20:39:29 +0200

    Dodanie czcionki i testowego napisu na ekranie

--- ProjektTestowy/Game1.cs
+++ ProjektTestowy/Game1.cs
@@ -11,6 +11,7 @@ namespace ProjektTestowy
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteFont DefaultFont;
 
         public Game1()
         {
@@ -18,23 +19,14 @@ namespace ProjektTestowy
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            DefaultFont = Content.Load<SpriteFont>("SpriteFontPL");
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -43,20 +35,11 @@ namespace ProjektTestowy
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -67,15 +50,16 @@ namespace ProjektTestowy
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            {
+                spriteBatch.Begin();
+                spriteBatch.DrawString(DefaultFont, "Hello World!", new Vector2(100, 50), Color.Black);
+                spriteBatch.End();
+            }
 
             base.Draw(gameTime);
         }
