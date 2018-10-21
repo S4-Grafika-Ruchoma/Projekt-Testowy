801cc688c13c09e17c7f85318c06e03908a2f1ea
Alan <alanbiegun13@gmail.com>
2018-10-21 23:14:41 +0200

    Wyłączenie interackcji po zmianie okna

--- ProjektTestowy/CustomClasses/Camera.cs
+++ ProjektTestowy/CustomClasses/Camera.cs
@@ -86,67 +86,71 @@ namespace ProjektTestowy.CustomClasses
 
         public override void Update(GameTime gameTime)
         {
-            currentMouseState = Mouse.GetState();
-            var keyboardState = Keyboard.GetState();
-            var dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            var moveVector = Vector3.Zero;
-
-            if (keyboardState.IsKeyDown(Keys.W))
-                moveVector.Z = 1;
-            if (keyboardState.IsKeyDown(Keys.S))
-                moveVector.Z = -1;
-
-            if (keyboardState.IsKeyDown(Keys.A))
-                moveVector.X = 1;
-            if (keyboardState.IsKeyDown(Keys.D))
-                moveVector.X = -1;
-
-            if (AllowClimb)
+            if (Game.IsActive)
             {
-                if (keyboardState.IsKeyDown(Keys.Space))
-                    moveVector.Y = 1;
-                if (keyboardState.IsKeyDown(Keys.C))
-                    moveVector.Y = -1;
+                currentMouseState = Mouse.GetState();
+                var keyboardState = Keyboard.GetState();
+                var dt = (float) gameTime.ElapsedGameTime.TotalSeconds;
+
+                var moveVector = Vector3.Zero;
+
+                if (keyboardState.IsKeyDown(Keys.W))
+                    moveVector.Z = 1;
+                if (keyboardState.IsKeyDown(Keys.S))
+                    moveVector.Z = -1;
+
+                if (keyboardState.IsKeyDown(Keys.A))
+                    moveVector.X = 1;
+                if (keyboardState.IsKeyDown(Keys.D))
+                    moveVector.X = -1;
+
+                if (AllowClimb)
+                {
+                    if (keyboardState.IsKeyDown(Keys.Space))
+                        moveVector.Y = 1;
+                    if (keyboardState.IsKeyDown(Keys.C))
+                        moveVector.Y = -1;
+                }
+
+                if (moveVector != Vector3.Zero)
+                {
+                    moveVector.Normalize();
+                    moveVector *= dt * cameraSpeed;
+
+                    Move(moveVector);
+                }
+
+                float deltaX, deltaY;
+
+                if (currentMouseState != prevMouseState)
+                {
+                    deltaX = currentMouseState.X - (Game.GraphicsDevice.Viewport.Width / 2);
+                    deltaY = currentMouseState.Y - (Game.GraphicsDevice.Viewport.Height / 2);
+
+                    mouseRotationBuffer.X -= 0.05F * deltaX * dt;
+                    mouseRotationBuffer.Y -= 0.05F * deltaY * dt;
+
+                    if (mouseRotationBuffer.Y < MathHelper.ToRadians(-75))
+                        mouseRotationBuffer.Y -= mouseRotationBuffer.Y - MathHelper.ToRadians(-75);
+
+                    if (mouseRotationBuffer.Y > MathHelper.ToRadians(75))
+                        mouseRotationBuffer.Y -= mouseRotationBuffer.Y - MathHelper.ToRadians(75);
+
+                    Rotation = new Vector3(
+                        -MathHelper.Clamp(mouseRotationBuffer.Y, MathHelper.ToRadians(-75f), MathHelper.ToRadians(75f)),
+                        MathHelper.WrapAngle(mouseRotationBuffer.X),
+                        0);
+
+                    deltaX = 0;
+                    deltaY = 0;
+                }
+
+                Mouse.SetPosition(Game.GraphicsDevice.Viewport.Width / 2, Game.GraphicsDevice.Viewport.Height / 2);
+
+                prevMouseState = currentMouseState;
+
+                base.Update(gameTime);
             }
-
-            if (moveVector != Vector3.Zero)
-            {
-                moveVector.Normalize();
-                moveVector *= dt * cameraSpeed;
-
-                Move(moveVector);
-            }
-
-            float deltaX, deltaY;
-
-            if (currentMouseState != prevMouseState)
-            {
-                deltaX = currentMouseState.X - (Game.GraphicsDevice.Viewport.Width / 2);
-                deltaY = currentMouseState.Y - (Game.GraphicsDevice.Viewport.Height / 2);
-
-                mouseRotationBuffer.X -= 0.05F * deltaX * dt;
-                mouseRotationBuffer.Y -= 0.05F * deltaY * dt;
-
-                if (mouseRotationBuffer.Y < MathHelper.ToRadians(-75))
-                    mouseRotationBuffer.Y -= mouseRotationBuffer.Y - MathHelper.ToRadians(-75);
-
-                if (mouseRotationBuffer.Y > MathHelper.ToRadians(75))
-                    mouseRotationBuffer.Y -= mouseRotationBuffer.Y - MathHelper.ToRadians(75);
-
-                Rotation = new Vector3(
-                    -MathHelper.Clamp(mouseRotationBuffer.Y,MathHelper.ToRadians(-75f),MathHelper.ToRadians(75f)),
-                    MathHelper.WrapAngle(mouseRotationBuffer.X),
-                    0);
-
-                deltaX = 0;
-                deltaY = 0;
-            }
-            Mouse.SetPosition(Game.GraphicsDevice.Viewport.Width / 2, Game.GraphicsDevice.Viewport.Height / 2);
-
-            prevMouseState = currentMouseState;
-
-            base.Update(gameTime);
         }
     }
 }
